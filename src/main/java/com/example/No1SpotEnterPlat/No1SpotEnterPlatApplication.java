26eb9828c86cfb48e0df6b512ccf5725ac6c5c0b
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-10-21 12:38:59 +0100

    First Commit

---
+++ src/main/java/com/example/No1SpotEnterPlat/No1SpotEnterPlatApplication.java
@@ -0,0 +1,13 @@
+package com.example.No1SpotEnterPlat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No1SpotEnterPlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(No1SpotEnterPlatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/No1SpotEnterPlat/No1SpotEnterPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.No1SpotEnterPlat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class No1SpotEnterPlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
