c9cf8fb7b3f8ee91be1de120fd93a05b750292fa
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-30 17:16:53 +0100

    User mgt Service

---
+++ services/customer/src/main/java/dev/innocent/ecomvista/customer/Customer.java
@@ -0,0 +1,20 @@
+package dev.innocent.ecomvista.customer;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Customer {
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Address address;
+}
