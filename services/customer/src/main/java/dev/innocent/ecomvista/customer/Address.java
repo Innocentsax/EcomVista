6e93d1928678f907356394eed0b6a81a0e23dc85
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-30 17:16:03 +0100

    User mgt Service

---
+++ services/customer/src/main/java/dev/innocent/ecomvista/customer/Address.java
@@ -0,0 +1,16 @@
+package dev.innocent.ecomvista.customer;
+
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Validated
+public class Address {
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+}
