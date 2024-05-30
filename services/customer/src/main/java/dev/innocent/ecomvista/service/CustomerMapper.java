1e54d13a2e3ef19e09ed1d1c42deb205ee6bff99
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-30 17:16:34 +0100

    User mgt Service

---
+++ services/customer/src/main/java/dev/innocent/ecomvista/service/CustomerMapper.java
@@ -0,0 +1,32 @@
+package dev.innocent.ecomvista.service;
+
+import dev.innocent.ecomvista.DTO.request.CustomerRequest;
+import dev.innocent.ecomvista.DTO.response.CustomerResponse;
+import dev.innocent.ecomvista.customer.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMapper {
+    public Customer toCustomer(CustomerRequest request) {
+        if(request == null){
+            return null;
+        }
+        return Customer.builder()
+                .id(request.id())
+                .firstname(request.firstname())
+                .lastname(request.lastname())
+                .email(request.email())
+                .address(request.address())
+                .build();
+    }
+
+    public CustomerResponse fromCustomer(Customer customer) {
+        return new CustomerResponse(
+                customer.getId(),
+                customer.getFirstname(),
+                customer.getLastname(),
+                customer.getEmail(),
+                customer.getAddress()
+        );
+    }
+}
