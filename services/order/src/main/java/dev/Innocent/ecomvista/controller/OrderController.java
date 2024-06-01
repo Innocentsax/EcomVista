3c3dd5eb425458e26be58dcdde8278ad52c88240
Innocent Udo <innocentcharlesudo@gmail.com>
2024-06-01 18:51:47 +0100

    Order-Services

--- services/order/src/main/java/dev/Innocent/ecomvista/controller/OrderController.java
+++ services/order/src/main/java/dev/Innocent/ecomvista/controller/OrderController.java
@@ -1,14 +1,14 @@
 package dev.Innocent.ecomvista.controller;
 
 import dev.Innocent.ecomvista.DTO.request.OrderRequest;
+import dev.Innocent.ecomvista.DTO.response.OrderResponse;
 import dev.Innocent.ecomvista.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +20,15 @@ public class OrderController {
     public ResponseEntity<Integer> createOrder(
             @RequestBody @Valid OrderRequest orderRequest){
         return ResponseEntity.ok(orderService.createOrder(orderRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAll(){
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    @GetMapping("/{order-id}")
+    public ResponseEntity<OrderResponse> findById(@PathVariable("order-id") Integer orderId){
+        return ResponseEntity.ok(orderService.findById(orderId));
     }
 }
