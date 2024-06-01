59973201fe52cb5b27c49e78088e553908448620
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 16:41:54 +0100

    Order mgt Service

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/DTO/response/CustomerResponse.java
@@ -0,0 +1,9 @@
+package dev.Innocent.ecomvista.DTO.response;
+
+public record CustomerResponse(
+        String id,
+        String firstName,
+        String lastName,
+        String email
+) {
+}
