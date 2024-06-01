21d5e290b379f88291ef8ab50373074d90c86634
Innocent Udo <innocentcharlesudo@gmail.com>
2024-06-01 18:51:56 +0100

    Order-Services

--- services/order/src/main/java/dev/Innocent/ecomvista/repository/OrderLineRepository.java
+++ services/order/src/main/java/dev/Innocent/ecomvista/repository/OrderLineRepository.java
@@ -3,5 +3,8 @@ package dev.Innocent.ecomvista.repository;
 import dev.Innocent.ecomvista.model.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
+    List<OrderLine> findAllByOrderId(Integer orderId);
 }
