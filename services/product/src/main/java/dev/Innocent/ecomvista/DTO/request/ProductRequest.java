ec762c33fe0d73fb62aec2922f7f587302cd73d8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 04:42:23 +0100

    product mgt Service

---
+++ services/product/src/main/java/dev/Innocent/ecomvista/DTO/request/ProductRequest.java
@@ -0,0 +1,27 @@
+package dev.Innocent.ecomvista.DTO.request;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ProductRequest (
+        Integer id,
+
+        @NotNull(message = "Product name is required")
+        String name,
+
+        @NotNull(message = "Product description is required")
+        String description,
+
+        @Positive(message = "Product available quantity must be greater than 0")
+        Double availableQuantity,
+
+        @Positive(message = "Product price must be greater than 0")
+        BigDecimal price,
+
+        @NotNull(message = "Product category is required")
+        Integer categoryId
+) {
+}
