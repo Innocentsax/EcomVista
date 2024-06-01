10e47a1f993bc79cbfaa8d0ba4c474d6ae2d5dee
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 04:41:56 +0100

    product mgt Service

---
+++ services/product/src/main/java/dev/Innocent/ecomvista/product/Product.java
@@ -0,0 +1,25 @@
+package dev.Innocent.ecomvista.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private Double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
