8df3ef941ddd450232de17b750d397f9136c2a16
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 11:20:13 +0100

    product mgt Service

---
+++ services/product/src/main/java/dev/Innocent/ecomvista/exception/ProductPurchaseException.java
@@ -0,0 +1,7 @@
+package dev.Innocent.ecomvista.exception;
+
+public class ProductPurchaseException extends RuntimeException{
+    public ProductPurchaseException(String message) {
+        super(message);
+    }
+}
