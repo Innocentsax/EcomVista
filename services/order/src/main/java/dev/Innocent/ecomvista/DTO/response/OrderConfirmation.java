f3a908b8dff00271b5ff30c7a839fa2630bce012
Innocent Udo <innocentcharlesudo@gmail.com>
2024-06-01 18:51:42 +0100

    Order-Services

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/DTO/response/OrderConfirmation.java
@@ -0,0 +1,15 @@
+package dev.Innocent.ecomvista.DTO.response;
+
+import dev.Innocent.ecomvista.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        CustomerResponse customerResponse,
+        List<PurchaseResponse> products
+) {
+}
