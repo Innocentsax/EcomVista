8c5bd89f1c0ab3e054c25e796f45c6f5a3b96572
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 16:43:13 +0100

    Order mgt Service

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/DTO/response/PurchaseResponse.java
@@ -0,0 +1,12 @@
+package dev.Innocent.ecomvista.DTO.response;
+
+import java.math.BigDecimal;
+
+public record PurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
