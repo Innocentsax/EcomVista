b509c08ef28e6f672d32771a9d6a6f9cc529c1ed
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 16:42:33 +0100

    Order mgt Service

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/service/OrderLineMapper.java
@@ -0,0 +1,20 @@
+package dev.Innocent.ecomvista.service;
+
+import dev.Innocent.ecomvista.DTO.request.OrderLineRequest;
+import dev.Innocent.ecomvista.model.Order;
+import dev.Innocent.ecomvista.model.OrderLine;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLineMapper {
+    public OrderLine toOrderLine(OrderLineRequest orderLineRequest) {
+        return OrderLine.builder()
+                .id(orderLineRequest.id())
+                .productId(orderLineRequest.productId())
+                .quantity(orderLineRequest.quantity())
+                .order(Order.builder()
+                        .id(orderLineRequest.orderId())
+                        .build())
+                .build();
+    }
+}
