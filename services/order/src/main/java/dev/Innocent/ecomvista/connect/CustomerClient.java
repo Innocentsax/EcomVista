a041c212f154d63437d3465a482f60d4878887bc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 16:41:50 +0100

    Order mgt Service

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/connect/CustomerClient.java
@@ -0,0 +1,18 @@
+package dev.Innocent.ecomvista.connect;
+
+import dev.Innocent.ecomvista.DTO.response.CustomerResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(
+        name = "customer-service",
+        url = "${application.config.customer-url}"
+)
+public interface CustomerClient {
+
+    @GetMapping("/{customer-id}")
+    Optional<CustomerResponse> findCustomerById(@PathVariable("customer-id") String customerId);
+}
