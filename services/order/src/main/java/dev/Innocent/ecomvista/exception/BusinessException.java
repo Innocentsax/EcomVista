eec020229b5308772df79109938ca99e729662b8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 16:41:43 +0100

    Order mgt Service

---
+++ services/order/src/main/java/dev/Innocent/ecomvista/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package dev.Innocent.ecomvista.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BusinessException extends RuntimeException {
+    private final String message;
+}
