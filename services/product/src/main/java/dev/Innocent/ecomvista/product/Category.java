f6d8af49d8e93112c61959a0f2a669ce7466fdca
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 04:39:49 +0100

    product mgt Service

---
+++ services/product/src/main/java/dev/Innocent/ecomvista/product/Category.java
@@ -0,0 +1,22 @@
+package dev.Innocent.ecomvista.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
