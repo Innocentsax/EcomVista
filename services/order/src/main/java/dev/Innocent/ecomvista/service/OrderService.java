103b87abf5287fbf2233c3011c75f6dfb68f0f1b
Innocent Udo <innocentcharlesudo@gmail.com>
2024-06-01 18:52:05 +0100

    Order-Services

--- services/order/src/main/java/dev/Innocent/ecomvista/service/OrderService.java
+++ services/order/src/main/java/dev/Innocent/ecomvista/service/OrderService.java
@@ -3,13 +3,20 @@ package dev.Innocent.ecomvista.service;
 import dev.Innocent.ecomvista.DTO.request.OrderLineRequest;
 import dev.Innocent.ecomvista.DTO.request.OrderRequest;
 import dev.Innocent.ecomvista.DTO.request.PurchaseRequest;
+import dev.Innocent.ecomvista.DTO.response.OrderConfirmation;
+import dev.Innocent.ecomvista.DTO.response.OrderResponse;
+import dev.Innocent.ecomvista.config.KafkaOrderProducer;
 import dev.Innocent.ecomvista.connect.CustomerClient;
 import dev.Innocent.ecomvista.connect.ProductClient;
 import dev.Innocent.ecomvista.exception.BusinessException;
 import dev.Innocent.ecomvista.model.OrderLine;
 import dev.Innocent.ecomvista.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +26,15 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper mapper;
     private final OrderLineService orderLineService;
+    private final KafkaOrderProducer kafkaOrderProducer;
 
     public Integer createOrder(OrderRequest orderRequest) {
         // Check the customer --> call customer service using feign client
         var customer = this.customerClient.findCustomerById(orderRequest.customerId())
-                .orElseThrow(() -> new BusinessException("Cannot create order:: Customer not found with id " + orderRequest.customerId());
+                .orElseThrow(() -> new BusinessException("Cannot create order:: Customer not found with id " + orderRequest.customerId()));
 
         // Purchase the product --> call product service using RestTemplate
-        this.productClient.purchaseProduct(orderRequest.products());
+        var purchaseProducts = this.productClient.purchaseProduct(orderRequest.products());
 
         // Save the order or persist the order
         var order = this.orderRepository.save(mapper.toOrder(orderRequest));
@@ -46,5 +54,29 @@ public class OrderService {
         // todo start Payment process
 
         // send the order confirmation -> notification service (kafta)
+        kafkaOrderProducer.sendOrderConfirmation(
+                new OrderConfirmation(
+                        orderRequest.reference(),
+                        orderRequest.amount(),
+                        orderRequest.paymentMethod(),
+                        customer,
+                        purchaseProducts
+                )
+        );
+
+        return order.getId();
+    }
+
+    public List<OrderResponse> findAll() {
+        return orderRepository.findAll().stream()
+                .map(mapper::fromOrder)
+                .collect(Collectors.toList());
+    }
+
+    public OrderResponse findById(Integer orderId) {
+        return orderRepository.findById(orderId)
+                .map(mapper::fromOrder)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("No Order found with the provided ID %d", orderId)));
     }
 }
