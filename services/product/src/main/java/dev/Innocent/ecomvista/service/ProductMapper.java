4b92b79aa54f034f29654a197bca0a11ddf8d23d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-01 11:20:16 +0100

    product mgt Service

--- services/product/src/main/java/dev/Innocent/ecomvista/service/ProductMapper.java
+++ services/product/src/main/java/dev/Innocent/ecomvista/service/ProductMapper.java
@@ -1,6 +1,8 @@
 package dev.Innocent.ecomvista.service;
 
 import dev.Innocent.ecomvista.DTO.request.ProductRequest;
+import dev.Innocent.ecomvista.DTO.response.ProductPurchaseResponse;
+import dev.Innocent.ecomvista.DTO.response.ProductResponse;
 import dev.Innocent.ecomvista.product.Category;
 import dev.Innocent.ecomvista.product.Product;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,28 @@ public class ProductMapper {
                         .id(productRequest.categoryId())
                         .build())
                 .build();
+    }
+
+    public ProductResponse toProductResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getAvailableQuantity(),
+                product.getPrice(),
+                product.getCategory().getId(),
+                product.getCategory().getName(),
+                product.getCategory().getDescription()
+        );
+    }
+
+    public ProductPurchaseResponse toProductPurchaseResponse(Product product, double quantity) {
+        return new ProductPurchaseResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                quantity
+        );
     }
 }
