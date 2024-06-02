f0ea0960c174dd28013ead9a6fb28cd55548e0a9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 04:12:49 +0100

    Notification mgt Service

---
+++ services/notification/src/main/java/dev/Innocent/notification/kafka/order/Product.java
@@ -0,0 +1,12 @@
+package dev.Innocent.notification.kafka.order;
+
+import java.math.BigDecimal;
+
+public record Product(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
