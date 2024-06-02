92d46d0b6e56e91739e4afdaba2d862c2ec7abd0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 04:11:59 +0100

    Notification mgt Service

---
+++ services/notification/src/main/java/dev/Innocent/notification/kafka/payment/PaymentConfirmation.java
@@ -0,0 +1,15 @@
+package dev.Innocent.notification.kafka.payment;
+
+import dev.Innocent.notification.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentConfirmation(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
