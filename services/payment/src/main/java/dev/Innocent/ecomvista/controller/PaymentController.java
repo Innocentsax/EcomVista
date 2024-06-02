98ff9d76f6a915d18c7f2a9e5c20b220be327745
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 01:47:20 +0100

    Payment mgt Service

---
+++ services/payment/src/main/java/dev/Innocent/ecomvista/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package dev.Innocent.ecomvista.controller;
+
+import dev.Innocent.ecomvista.DTO.request.PaymentRequest;
+import dev.Innocent.ecomvista.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createPayment(@RequestBody @Valid PaymentRequest paymentRequest) {
+        return ResponseEntity.ok(paymentService.createPayment(paymentRequest));
+    }
+}
