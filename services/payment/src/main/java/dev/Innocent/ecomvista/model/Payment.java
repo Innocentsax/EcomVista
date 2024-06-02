473f40afb28c9d3b4e793a47c7bf75ed1b610d6c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 01:47:00 +0100

    Payment mgt Service

---
+++ services/payment/src/main/java/dev/Innocent/ecomvista/model/Payment.java
@@ -0,0 +1,36 @@
+package dev.Innocent.ecomvista.model;
+
+import dev.Innocent.ecomvista.enums.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    private Integer orderId;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
