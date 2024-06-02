c2dbe0797019755f806150030cdd6f46bcf6a11d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 01:46:46 +0100

    Payment mgt Service

--- services/payment/src/main/java/dev/Innocent/ecomvista/PaymentApplication.java
+++ services/payment/src/main/java/dev/Innocent/ecomvista/PaymentApplication.java
@@ -2,8 +2,10 @@ package dev.Innocent.ecomvista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PaymentApplication {
 
 	public static void main(String[] args) {
