6de981b480e8d70b788602c83063c7005aeb486f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 01:47:26 +0100

    Payment mgt Service

---
+++ services/payment/src/main/java/dev/Innocent/ecomvista/model/Customer.java
@@ -0,0 +1,20 @@
+package dev.Innocent.ecomvista.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public record Customer(
+        String id,
+        @NotNull(message = "First name is required")
+        String firstName,
+
+        @NotNull(message = "Last name is required")
+        String lastName,
+
+        @NotNull(message = "Email is required")
+        @Email(message = "Email is invalid")
+        String email
+) {
+}
