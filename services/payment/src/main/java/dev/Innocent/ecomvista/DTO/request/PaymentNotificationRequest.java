e13f7f16cbdfdf2968fe5e39d21c428bb6075b47
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-02 01:46:54 +0100

    Payment mgt Service

---
+++ services/payment/src/main/java/dev/Innocent/ecomvista/DTO/request/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package dev.Innocent.ecomvista.DTO.request;
+
+import dev.Innocent.ecomvista.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
