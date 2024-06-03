12f0cf83ef70ad440c8b83c8bca75a0c4567bf53
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-03 09:12:52 +0100

    Notification mgt Service

--- services/payment/src/main/java/dev/Innocent/ecomvista/controller/PaymentController.java
+++ services/payment/src/main/java/dev/Innocent/ecomvista/controller/PaymentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/payment")
+@RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
