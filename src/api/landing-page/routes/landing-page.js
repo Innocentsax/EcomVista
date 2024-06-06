bb972853665c560ec26f6854bc64f79e7b3b94bf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-06 18:18:45 +0100

    Strapi Demo

---
+++ src/api/landing-page/routes/landing-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * landing-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::landing-page.landing-page');
