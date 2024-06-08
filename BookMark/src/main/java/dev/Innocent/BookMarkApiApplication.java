6b774091321de903b4d1cfe53e03a4c3eed5ee97
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-08 08:43:18 +0100

    BookMark Setup

---
+++ BookMark/src/main/java/dev/Innocent/BookMarkApiApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMarkApiApplication.class, args);
+	}
+
+}
