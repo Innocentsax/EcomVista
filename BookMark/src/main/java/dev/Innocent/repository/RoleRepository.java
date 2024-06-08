636a09baec00340d43214e5355047f5b53618a39
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-08 19:30:09 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String name);
+}
