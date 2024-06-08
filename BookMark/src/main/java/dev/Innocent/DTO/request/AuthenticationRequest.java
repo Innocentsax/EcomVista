1b37abe441855e103db567a1f586843ba4fa3e31
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-08 19:29:19 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/DTO/request/AuthenticationRequest.java
@@ -0,0 +1,24 @@
+package dev.Innocent.DTO.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationRequest {
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
+    private String email;
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+}
