303f7441309f71a5b67995b57ce7a03ae7381fd9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-08 19:30:04 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package dev.Innocent.security;
+
+import dev.Innocent.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
