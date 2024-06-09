72e75ea61471faf826c1a5d75b8f9cc61b30f7ee
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 12:45:09 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/service/impl/AuthenticationServiceImpl.java
+++ BookMark/src/main/java/dev/Innocent/service/impl/AuthenticationServiceImpl.java
@@ -3,6 +3,8 @@ package dev.Innocent.service.impl;
 import dev.Innocent.DTO.request.AuthenticationRequest;
 import dev.Innocent.DTO.request.RegistrationRequest;
 import dev.Innocent.DTO.response.AuthenticationResponse;
+import dev.Innocent.enums.EmailTemplateName;
+import dev.Innocent.model.Token;
 import dev.Innocent.model.User;
 import dev.Innocent.repository.RoleRepository;
 import dev.Innocent.repository.TokenRepository;
@@ -11,9 +13,16 @@ import dev.Innocent.security.JwtService;
 import dev.Innocent.service.AuthenticationService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,13 +30,29 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
-    private final AuthenticationProvider authenticationProvider;
+    private final EmailServiceImp emailService;
+    private final AuthenticationProvider authenticationManager;
     private final RoleRepository roleRepository;
     private final TokenRepository tokenRepository;
 
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
     @Override
     public void register(RegistrationRequest request) throws MessagingException {
-
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initiated"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
     }
 
     @Override
@@ -37,21 +62,62 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public String generateAndSaveActivationToken(User user) {
-        return "";
+        // Generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
     }
 
     @Override
     public void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
 
+        emailService.sendEmail(
+                user.getEmail(),
+                user.getFullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+        );
     }
 
     @Override
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var claims = new HashMap<String, Object>();
+        var user = ((User) auth.getPrincipal());
+        claims.put("fullName", user.getFullName());
+
+        var jwtToken = jwtService.generateToken(claims, (User) auth.getPrincipal());
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     @Override
     public String generateActivationCode(int length) {
-        return "";
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
