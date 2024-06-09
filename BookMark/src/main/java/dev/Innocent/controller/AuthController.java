7ef399fa391579d7ab87e00e119e542515a21ceb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 12:44:52 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/controller/AuthController.java
+++ BookMark/src/main/java/dev/Innocent/controller/AuthController.java
@@ -1,10 +1,16 @@
 package dev.Innocent.controller;
 
+import dev.Innocent.DTO.request.AuthenticationRequest;
+import dev.Innocent.DTO.request.RegistrationRequest;
+import dev.Innocent.DTO.response.AuthenticationResponse;
 import dev.Innocent.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -13,5 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationService service;
 
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request) throws MessagingException {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
 
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody @Valid AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
 }
