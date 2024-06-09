2bfef3a5e37afe5cf9d81d526a2021a130052c30
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 20:22:09 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/handler/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package dev.Innocent.handler;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
