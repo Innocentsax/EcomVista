e2ffb2ab6f1615a5a38956c9100a41518e25d80d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 20:22:44 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/Mappers/BookMapper.java
@@ -0,0 +1,50 @@
+package dev.Innocent.Mappers;
+
+import dev.Innocent.DTO.request.BookRequest;
+import dev.Innocent.DTO.response.BookResponse;
+import dev.Innocent.DTO.response.BorrowedBookResponse;
+import dev.Innocent.model.Book;
+import dev.Innocent.model.BookTransactionHistory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .isbn(request.isbn())
+                .authorName(request.authorName())
+                .synopsis(request.synopsis())
+                .archived(false)
+                .shareable(request.shareable())
+                .build();
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsis(book.getSynopsis())
+                .rate(book.getRate())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().getFullName())
+//                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
+                .build();
+    }
+
+    public BorrowedBookResponse toBorrowedBookResponse(BookTransactionHistory history) {
+        return BorrowedBookResponse.builder()
+                .id(history.getBook().getId())
+                .title(history.getBook().getTitle())
+                .authorName(history.getBook().getAuthorName())
+                .isbn(history.getBook().getIsbn())
+                .rate(history.getBook().getRate())
+                .returned(history.isReturned())
+                .returnApproved(history.isReturnApproved())
+                .build();
+    }
+}
