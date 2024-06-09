60b2d458d7dfcd1d0d9afe71262a63082d10ba20
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 20:22:48 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/BookMarkApiApplication.java
+++ BookMark/src/main/java/dev/Innocent/BookMarkApiApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class BookMarkApiApplication {
 
