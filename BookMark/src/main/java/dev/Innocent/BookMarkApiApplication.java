3117356a07df0f675fc5d115a9460da56a4106d0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-09 12:45:05 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/BookMarkApiApplication.java
+++ BookMark/src/main/java/dev/Innocent/BookMarkApiApplication.java
@@ -1,15 +1,30 @@
 package dev.Innocent;
 
+import dev.Innocent.model.Role;
+import dev.Innocent.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BookMarkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookMarkApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
 	}
 
 }
