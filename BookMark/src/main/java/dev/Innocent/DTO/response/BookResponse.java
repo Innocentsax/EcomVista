eba742765a07e76cf204e723df246a5dc30cc5a1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:07:52 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/DTO/response/BookResponse.java
@@ -0,0 +1,27 @@
+package dev.Innocent.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookResponse {
+
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String owner;
+    private byte[] cover;
+    private double rate;
+    private boolean archived;
+    private boolean shareable;
+
+}
