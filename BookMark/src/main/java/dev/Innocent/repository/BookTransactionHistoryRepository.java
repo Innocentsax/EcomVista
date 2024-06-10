d5e96564497da6e1fa71b7ab7eb96977d7a9c6ce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:08:25 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/repository/BookTransactionHistoryRepository.java
@@ -0,0 +1,23 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.BookTransactionHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.user.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.owner.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+}
