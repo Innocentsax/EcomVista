a64c624ab2b893f5c2db30f9ad3f2c655d2d8549
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:08:18 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/model/BookTransactionHistory.java
@@ -0,0 +1,29 @@
+package dev.Innocent.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+    private boolean returnApproved;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
