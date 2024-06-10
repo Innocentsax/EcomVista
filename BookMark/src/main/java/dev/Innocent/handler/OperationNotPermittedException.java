c4da9b0d051e2cbc7241f16ed465b96cb47b4bc3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:08:14 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/handler/OperationNotPermittedException.java
@@ -0,0 +1,11 @@
+package dev.Innocent.handler;
+
+public class OperationNotPermittedException extends RuntimeException {
+
+    public OperationNotPermittedException() {
+    }
+
+    public OperationNotPermittedException(String message) {
+        super(message);
+    }
+}
