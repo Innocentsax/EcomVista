41b1bac7c7dbe49c3c06aa5218ab743854518c6d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:08:38 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/handler/ExceptionResponse.java
@@ -0,0 +1,21 @@
+package dev.Innocent.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
