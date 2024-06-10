f40b1535a67942f7fa3d7ecde839afeeb49575ce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-10 08:08:30 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/model/Feedback.java
@@ -0,0 +1,27 @@
+package dev.Innocent.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    @Column
+    private Double note; // 0 to 5
+    private String comment;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
