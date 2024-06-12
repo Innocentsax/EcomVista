19e16698143ac3e5f08c48e0436d22ea0274793a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-12 17:10:06 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/controller/BookController.java
+++ BookMark/src/main/java/dev/Innocent/controller/BookController.java
@@ -77,4 +77,18 @@ public class BookController {
     ) {
         return ResponseEntity.ok(service.updateShareableStatus(bookId, connectedUser));
     }
+
+    @PatchMapping("/archived/{book-id}")
+    public ResponseEntity<Integer> updateArchivedStatus(@PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.updateArchivedStatus(bookId, connectedUser));
+    }
+
+    @PostMapping("borrow/{book-id}")
+    public ResponseEntity<Integer> borrowBook(@PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.borrowBook(bookId, connectedUser));
+    }
 }
