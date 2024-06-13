505ed705409ca6b91799a1ecd0aae7fa2fee2c18
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-13 16:51:22 +0100

    BookMark

--- BookMark/src/main/java/dev/Innocent/repository/BookTransactionHistoryRepository.java
+++ BookMark/src/main/java/dev/Innocent/repository/BookTransactionHistoryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
     @Query("""
             SELECT history
@@ -40,4 +42,24 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             AND bookTransactionHistory.returnApproved = false
             """)
     boolean isAlreadyBorrowed(@Param("bookId") Integer bookId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory  transaction
+            WHERE transaction.user.id = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = false
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndUserId(@Param("bookId") Integer bookId, @Param("userId") Integer userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory  transaction
+            WHERE transaction.book.owner.id = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = true
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(@Param("bookId") Integer bookId, @Param("userId") Integer userId);
 }
