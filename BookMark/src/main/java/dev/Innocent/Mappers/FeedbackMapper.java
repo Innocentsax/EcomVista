32aa79c01c49abac567abb423c967f9e58b69c30
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-14 10:14:22 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/Mappers/FeedbackMapper.java
@@ -0,0 +1,33 @@
+package dev.Innocent.Mappers;
+
+import dev.Innocent.DTO.request.FeedbackRequest;
+import dev.Innocent.DTO.response.FeedbackResponse;
+import dev.Innocent.model.Book;
+import dev.Innocent.model.Feedback;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .shareable(false) // Not required and has no impact :: just to satisfy lombok
+                        .archived(false) // Not required and has no impact :: just to satisfy lombok
+                        .build()
+                )
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
+    }
+}
