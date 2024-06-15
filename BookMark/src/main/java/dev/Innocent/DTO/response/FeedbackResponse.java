08313f08c89bb118ca2736ba82543be0d63f3cd9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-15 14:41:22 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/DTO/response/FeedbackResponse.java
@@ -0,0 +1,19 @@
+package dev.Innocent.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FeedbackResponse {
+
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
