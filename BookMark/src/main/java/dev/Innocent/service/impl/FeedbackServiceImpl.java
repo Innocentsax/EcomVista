766eea16061b831da1f4a6a7de31470340d30493
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-16 13:41:32 +0100

    BookMark

---
+++ BookMark/src/main/java/dev/Innocent/service/impl/FeedbackServiceImpl.java
@@ -0,0 +1,67 @@
+package dev.Innocent.service.impl;
+
+import dev.Innocent.DTO.request.FeedbackRequest;
+import dev.Innocent.DTO.response.FeedbackResponse;
+import dev.Innocent.DTO.response.PageResponse;
+import dev.Innocent.Mappers.FeedbackMapper;
+import dev.Innocent.handler.OperationNotPermittedException;
+import dev.Innocent.model.Book;
+import dev.Innocent.model.Feedback;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.BookRepository;
+import dev.Innocent.repository.FeedBackRepository;
+import dev.Innocent.service.FeedbackService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackServiceImpl implements FeedbackService {
+
+    private final FeedBackRepository feedBackRepository;
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+        User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedBackRepository.save(feedback).getId();
+    }
+
+    @Transactional
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size);
+        User user = ((User) connectedUser.getPrincipal());
+        Page<Feedback> feedbacks = feedBackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(f -> feedbackMapper.toFeedbackResponse(f, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+
+    }
+}
