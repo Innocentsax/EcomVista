a5f98f971ecfd5e47ab5345747f1dc6f31b0cfdf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-17 08:45:41 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/VikingTreadingApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikingTreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikingTreadingApplication.class, args);
+	}
+
+}
