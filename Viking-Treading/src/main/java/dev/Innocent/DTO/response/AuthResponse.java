f8a7fdcde741a84720ea3290ad2a34061fd68429
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-18 00:11:22 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/DTO/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package dev.Innocent.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String jwt;
+    private String message;
+    private boolean status;
+    private boolean isTwoFactorAuthEnabled;
+    private String session;
+}
