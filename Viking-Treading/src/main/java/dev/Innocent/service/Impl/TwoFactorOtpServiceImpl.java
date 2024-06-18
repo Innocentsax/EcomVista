518faa80ce41c426b6d541cc809576b36f54bea9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-18 08:07:59 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/service/Impl/TwoFactorOtpServiceImpl.java
@@ -0,0 +1,51 @@
+package dev.Innocent.service.Impl;
+
+import dev.Innocent.model.TwoFactorOTP;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.TwoFactorOtpRepository;
+import dev.Innocent.service.TwoFactorOtpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TwoFactorOtpServiceImpl implements TwoFactorOtpService {
+    private final TwoFactorOtpRepository twoFactorOtpRepository;
+
+    @Override
+    public TwoFactorOTP createTwoFactorOtp(User user, String otp, String jwt) {
+        UUID uuid = UUID.randomUUID();
+        String id = uuid.toString();
+
+        TwoFactorOTP twoFactorOTP = new TwoFactorOTP();
+        twoFactorOTP.setId(id);
+        twoFactorOTP.setOtp(otp);
+        twoFactorOTP.setUser(user);
+        twoFactorOTP.setJwt(jwt);
+        return twoFactorOtpRepository.save(twoFactorOTP);
+    }
+
+    @Override
+    public TwoFactorOTP findByUser(Long userId) {
+        return twoFactorOtpRepository.findByUserId(userId);
+    }
+
+    @Override
+    public TwoFactorOTP findById(String id) {
+        Optional<TwoFactorOTP> twoFactorOTP = twoFactorOtpRepository.findById(id);
+        return twoFactorOTP.orElse(null);
+    }
+
+    @Override
+    public boolean verifyTwoFactorOtp(TwoFactorOTP twoFactorOTP, String otp) {
+        return twoFactorOTP.getOtp().equals(otp);
+    }
+
+    @Override
+    public void deleteTwoFactorOtp(TwoFactorOTP twoFactorOTP) {
+        twoFactorOtpRepository.delete(twoFactorOTP);
+    }
+}
