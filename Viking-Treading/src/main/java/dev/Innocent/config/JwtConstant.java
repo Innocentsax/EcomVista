5b3b360ad10eb37402eb2e839a594933cb209a34
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-18 08:08:39 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package dev.Innocent.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "jdfnekjdbnfxknvlbljfmsbnfjfjbslfnsjfblwngjrbvkjfhdbvkdjfksnjllflnglsjlrbgjsrfkjdsflkjnjnfbfklj";
+    public static final String JWT_HEADER = "Authorization";
+}
