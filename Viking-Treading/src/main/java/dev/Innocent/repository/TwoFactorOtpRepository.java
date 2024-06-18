200f054fcdd8c4f5821d55f832e08387091d5499
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-18 08:08:06 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/TwoFactorOtpRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.TwoFactorOTP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TwoFactorOtpRepository extends JpaRepository<TwoFactorOTP, String> {
+    TwoFactorOTP findByUserId(Long userId);
+}
