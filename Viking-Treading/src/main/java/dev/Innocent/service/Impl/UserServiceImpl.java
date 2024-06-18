1550f4aedee0c6822cb6d815c399085e37f2453f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-18 08:07:34 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/service/Impl/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package dev.Innocent.service.Impl;
+
+import dev.Innocent.config.JwtProvider;
+import dev.Innocent.enums.VerificationType;
+import dev.Innocent.model.TwoFactorAuth;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.UserRepository;
+import dev.Innocent.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserProfileByJwt(String jwt) throws Exception {
+        String email = jwtProvider.getEmailFromJwtToken(jwt);
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserById(Long userId) throws Exception {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()){
+            throw new Exception("User not found");
+        }
+        return user.get();
+    }
+
+    @Override
+    public User enableTwoFactorAuthentication(VerificationType verificationType, String sendTo, User user) {
+        TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
+        twoFactorAuth.setEnable(true);
+        twoFactorAuth.setSendTo(verificationType);
+
+        user.setTwoFactorAuth(twoFactorAuth);
+        return userRepository.save(user);
+    }
+
+
+    @Override
+    public User updatePassword(User user, String password) {
+        user.setPassword(password);
+        return userRepository.save(user);
+    }
+}
