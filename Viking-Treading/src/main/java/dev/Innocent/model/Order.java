b62edb1bf75239971f90acc75a8aa57edf4941b6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-20 20:21:49 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/model/Order.java
@@ -0,0 +1,15 @@
+package dev.Innocent.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
