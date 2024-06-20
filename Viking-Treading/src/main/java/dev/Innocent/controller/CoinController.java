90f056061fc7bd356fb6ce35be4aee0b1dd391b2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-21 00:50:20 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/controller/CoinController.java
+++ Viking-Treading/src/main/java/dev/Innocent/controller/CoinController.java
@@ -32,4 +32,32 @@ public class CoinController {
         JsonNode jsonNode = objectMapper.readTree(marketChart);
         return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
     }
+
+    @GetMapping("/search")
+    ResponseEntity<JsonNode> searchCoin(@RequestParam("q") String keyword) throws Exception {
+        String searchResult = coinService.searchCoin(keyword);
+        JsonNode jsonNode = objectMapper.readTree(searchResult);
+        return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/top50")
+    ResponseEntity<JsonNode> getTop50CoinByMarketCapBank() throws Exception {
+        String top50Coin = coinService.getTop50CoinsByMarketCapRank();
+        JsonNode jsonNode = objectMapper.readTree(top50Coin);
+        return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/treading")
+    ResponseEntity<JsonNode> getTreadingCoin() throws Exception {
+        String treadingCoin = coinService.getTreadingCoins();
+        JsonNode jsonNode = objectMapper.readTree(treadingCoin);
+        return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/details/{coinId}")
+    ResponseEntity<JsonNode> getCoinDetails(@PathVariable String coinId) throws Exception {
+        String coinDetails = coinService.getCoinDetails(coinId);
+        JsonNode jsonNode = objectMapper.readTree(coinDetails);
+        return new ResponseEntity<>(jsonNode, HttpStatus.ACCEPTED);
+    }
 }
