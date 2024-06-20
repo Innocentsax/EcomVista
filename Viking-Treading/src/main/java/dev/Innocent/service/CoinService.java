7448801e436f12afea97780aecb6ea32ff3d50aa
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-20 15:29:39 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/service/CoinService.java
@@ -0,0 +1,15 @@
+package dev.Innocent.service;
+
+import dev.Innocent.model.Coin;
+
+import java.util.List;
+
+public interface CoinService {
+    List<Coin> getCoinList(int page) throws Exception;
+    String getMarketChart(String coinId, int days) throws Exception;
+    String getCoinDetails(String coinId) throws Exception;
+    Coin findById(String coinId) throws Exception;
+    String searchCoin(String keyword) throws Exception;
+    String getTop50CoinsByMarketCapRank() throws Exception;
+    String GetTreadingCoins() throws Exception;
+}
