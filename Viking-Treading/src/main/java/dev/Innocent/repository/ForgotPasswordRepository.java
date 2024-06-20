be2bf052fe688f3209cadc9b5f209dd77e03e441
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-20 15:29:50 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/ForgotPasswordRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.ForgotPasswordToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken, String> {
+    ForgotPasswordToken findByUserId(Long userId);
+}
