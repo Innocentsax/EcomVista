705557866bee40e6bd0fd271479e01082f642045
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-22 06:35:30 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/service/CoinService.java
+++ Viking-Treading/src/main/java/dev/Innocent/service/CoinService.java
@@ -11,5 +11,5 @@ public interface CoinService {
     Coin findById(String coinId) throws Exception;
     String searchCoin(String keyword) throws Exception;
     String getTop50CoinsByMarketCapRank() throws Exception;
-    String GetTreadingCoins() throws Exception;
+    String getTreadingCoins() throws Exception;
 }
