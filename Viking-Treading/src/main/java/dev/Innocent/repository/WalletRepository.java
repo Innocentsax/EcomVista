951a450e9f7bd776f518aca547e2d90ee0b9b7a1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-23 12:37:09 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/WalletRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Wallet findByUserId(Long userId);
+}
