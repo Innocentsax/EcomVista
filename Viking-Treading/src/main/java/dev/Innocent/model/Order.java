14c2069cc659807605b58f6bb498ad400cc39eed
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-06-26 18:35:04 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/model/Order.java
+++ Viking-Treading/src/main/java/dev/Innocent/model/Order.java
@@ -1,15 +1,38 @@
 package dev.Innocent.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import dev.Innocent.enums.OrderStatus;
+import dev.Innocent.enums.OrderType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private OrderType orderType;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderItem orderItem;
 }
