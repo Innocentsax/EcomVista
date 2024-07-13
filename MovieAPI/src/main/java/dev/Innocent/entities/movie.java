a72e1da2daba2238250d774129f32cd07897ac86
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-13 23:08:29 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/entities/movie.java
@@ -0,0 +1,45 @@
+package dev.Innocent.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer movieId;
+
+    @Column(nullable = false, length = 200)
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Director is required")
+    private String director;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Studio is required")
+    private String studio;
+
+    @ElementCollection
+    @CollectionTable(name = "movie_cast")
+    private Set<String> movieCast;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Release year is required")
+    private Integer releaseYear;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Poster is required")
+    private String poster;
+}
