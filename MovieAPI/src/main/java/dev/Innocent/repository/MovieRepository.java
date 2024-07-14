11ec43ff4b6651e7dc20144053136a55b24af855
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-14 07:39:25 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
