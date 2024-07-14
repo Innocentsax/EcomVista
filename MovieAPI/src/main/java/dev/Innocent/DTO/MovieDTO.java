3048ca1d6b4ac89bdc1c115771fbc91c085c4574
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-14 07:39:22 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/DTO/MovieDTO.java
@@ -0,0 +1,25 @@
+package dev.Innocent.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieDTO {
+    @NotBlank(message = "Title is required")
+    private String title;
+    @NotBlank(message = "Director is required")
+    private String director;
+
+    @NotBlank(message = "Studio is required")
+    private String studio;
+    private Set<String> movieCast;
+    private Integer releaseYear;
+    @NotBlank(message = "Poster is required")
+    private String poster;
+}
