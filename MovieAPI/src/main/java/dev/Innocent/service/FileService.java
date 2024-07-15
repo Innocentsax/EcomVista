8d852edb3b7a839a00c8d62532406b21e4a00bbe
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-15 07:48:34 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/service/FileService.java
@@ -0,0 +1,12 @@
+package dev.Innocent.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface FileService {
+    String uploadFile(String path, MultipartFile file) throws IOException;
+    InputStream getResourceFile(String path, String name) throws FileNotFoundException;
+}
