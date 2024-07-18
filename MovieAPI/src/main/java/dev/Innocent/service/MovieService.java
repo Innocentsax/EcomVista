0850bd12bd92ffcf86a5badd1a6732a16ddb2c8d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-18 11:31:00 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/service/MovieService.java
+++ MovieAPI/src/main/java/dev/Innocent/service/MovieService.java
@@ -3,10 +3,11 @@ package dev.Innocent.service;
 import dev.Innocent.DTO.MovieDTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface MovieService {
-    MovieDTO addMovie(MovieDTO movieDTO, MultipartFile file);
+    MovieDTO addMovie(MovieDTO movieDTO, MultipartFile file) throws IOException;
     MovieDTO getMovie(Integer movieId);
     List<MovieDTO> getAllMovies();
 }
