0118d540ba58426bdcbf3a47e197c934d2d83f8a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-18 11:31:04 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/Config/SwaggerConfig.java
+++ MovieAPI/src/main/java/dev/Innocent/Config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 public class SwaggerConfig {
     @Bean
