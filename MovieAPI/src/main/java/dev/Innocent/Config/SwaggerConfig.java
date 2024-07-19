57359c915f995796090d11304163834a91c82480
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-19 16:52:18 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/Config/SwaggerConfig.java
+++ MovieAPI/src/main/java/dev/Innocent/Config/SwaggerConfig.java
@@ -2,14 +2,21 @@ package dev.Innocent.Config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("API").version("1.0"));
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
