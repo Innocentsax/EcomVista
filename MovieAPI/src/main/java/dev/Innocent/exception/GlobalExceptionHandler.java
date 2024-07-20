4b13ca5e3632eb2670e3e2431e2d20e2b1cfa586
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-20 20:29:43 +0100

    Global Exception

---
+++ MovieAPI/src/main/java/dev/Innocent/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package dev.Innocent.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FileExistException.class)
+    public ProblemDetail handleFileExistException(FileExistException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(EmptyFileException.class)
+    public ProblemDetail handleEmptyFileException(EmptyFileException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ProblemDetail handleMovieNotFoundException(MovieNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+}
