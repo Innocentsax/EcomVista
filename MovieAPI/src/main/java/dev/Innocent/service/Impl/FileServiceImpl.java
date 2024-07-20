212ea7d5506c9798030c5d24aaac51be1665287a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-20 10:14:55 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/service/Impl/FileServiceImpl.java
+++ MovieAPI/src/main/java/dev/Innocent/service/Impl/FileServiceImpl.java
@@ -28,7 +28,7 @@ public class FileServiceImpl implements FileService {
             fileObject.mkdirs();
         }
         // Copy file or upload file to the path
-        Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(file.getInputStream(), Paths.get(filePath));
         return fileName;
     }
 
