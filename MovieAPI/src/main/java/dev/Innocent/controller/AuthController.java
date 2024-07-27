a580bee9ea4a1db59981ff1c9dc152914f9c594b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-27 20:48:48 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/controller/AuthController.java
@@ -0,0 +1,56 @@
+package dev.Innocent.controller;
+
+import dev.Innocent.auth.model.RefreshToken;
+import dev.Innocent.auth.model.User;
+import dev.Innocent.auth.service.AuthService;
+import dev.Innocent.auth.service.JwtService;
+import dev.Innocent.auth.service.RefreshTokenService;
+import dev.Innocent.auth.util.AuthResponse;
+import dev.Innocent.auth.util.LoginRequest;
+import dev.Innocent.auth.util.RefreshTokenRequest;
+import dev.Innocent.auth.util.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth/")
+public class AuthController {
+
+    private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtService jwtService;
+
+    public AuthController(AuthService authService, RefreshTokenService refreshTokenService, JwtService jwtService) {
+        this.authService = authService;
+        this.refreshTokenService = refreshTokenService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+
+        RefreshToken refreshToken = refreshTokenService.verifyRefreshToken(refreshTokenRequest.getRefreshToken());
+        User user = refreshToken.getUser();
+
+        String accessToken = jwtService.generateToken(user);
+
+        return ResponseEntity.ok(AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getRefreshToken())
+                .build());
+    }
+}
