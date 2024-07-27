f5ee3d5537e5decd18f75e7b60d39349519e1d63
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-27 07:41:34 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/auth/service/AuthFilterService.java
@@ -0,0 +1,26 @@
+package dev.Innocent.auth.service;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthFilterService extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
