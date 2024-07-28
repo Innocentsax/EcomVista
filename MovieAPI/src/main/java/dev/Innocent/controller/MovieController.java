defd98a5a730e3adaf6adf7bfe7e980c39c2fead
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-28 14:02:46 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/controller/MovieController.java
+++ MovieAPI/src/main/java/dev/Innocent/controller/MovieController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,6 +24,7 @@ import java.util.List;
 public class MovieController {
     private final MovieService movieService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/add-movie")
     public ResponseEntity<MovieDTO> addMovieHandler(
             @RequestPart MultipartFile file, @RequestPart String movieDTO) throws IOException {
