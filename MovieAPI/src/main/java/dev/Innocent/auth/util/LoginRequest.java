7421181e433b23d6aa830838d48d5883bb250e6e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-28 14:02:42 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/auth/util/LoginRequest.java
@@ -0,0 +1,16 @@
+package dev.Innocent.auth.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
