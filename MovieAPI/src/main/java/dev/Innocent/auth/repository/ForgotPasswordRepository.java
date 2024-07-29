fc837ab2120ac0bad8e2e8ab8ce72dbbf3aefc8c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-29 07:35:47 +0100

    More about Endpoint

---
+++ MovieAPI/src/main/java/dev/Innocent/auth/repository/ForgotPasswordRepository.java
@@ -0,0 +1,14 @@
+package dev.Innocent.auth.repository;
+
+import dev.Innocent.auth.model.ForgotPassword;
+import dev.Innocent.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer> {
+
+    @Query("select fp from ForgotPassword fp where fp.otp = ?1 and fp.user = ?2")
+    Optional<ForgotPassword> findByOtpAndUser(Integer otp, User user);
+}
