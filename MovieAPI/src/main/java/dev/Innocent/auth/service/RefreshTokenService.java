1b1716b537733e3c9baf1569a1f0e5a8e99e3217
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-30 19:39:56 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/auth/service/RefreshTokenService.java
+++ MovieAPI/src/main/java/dev/Innocent/auth/service/RefreshTokenService.java
@@ -12,33 +12,46 @@ import java.time.Instant;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class RefreshTokenService {
-    private final RefreshTokenRepository refreshTokenRepository;
+
     private final UserRepository userRepository;
 
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshTokenService(UserRepository userRepository, RefreshTokenRepository refreshTokenRepository) {
+        this.userRepository = userRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
     public RefreshToken createRefreshToken(String username) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + username));
+
         RefreshToken refreshToken = user.getRefreshToken();
+
         if (refreshToken == null) {
-            long refreshTokenValidity = 5 * 60 * 60 * 10000;
+            long refreshTokenValidity = 30 * 1000;
             refreshToken = RefreshToken.builder()
                     .refreshToken(UUID.randomUUID().toString())
                     .expirationTime(Instant.now().plusMillis(refreshTokenValidity))
+                    .user(user)
                     .build();
+
             refreshTokenRepository.save(refreshToken);
         }
+
         return refreshToken;
     }
 
-    public RefreshToken validateRefreshToken(String token) {
-         RefreshToken refreshToken = refreshTokenRepository.findByRefreshToken(token)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid refresh Token"));
-        if (refreshToken.getExpirationTime().compareTo(Instant.now()) < 0) {
-            refreshTokenRepository.delete(refreshToken);
-            throw new IllegalArgumentException("Refresh token has expired");
+    public RefreshToken verifyRefreshToken(String refreshToken) {
+        RefreshToken refToken = refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new RuntimeException("Refresh token not found!"));
+
+        if (refToken.getExpirationTime().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(refToken);
+            throw new RuntimeException("Refresh Token expired");
         }
-        return refreshToken;
+
+        return refToken;
     }
 }
