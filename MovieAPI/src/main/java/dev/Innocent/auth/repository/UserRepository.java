9874e1fddf2428fe5623be63618bd8530663391e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-07-31 16:39:15 +0100

    More about Endpoint

--- MovieAPI/src/main/java/dev/Innocent/auth/repository/UserRepository.java
+++ MovieAPI/src/main/java/dev/Innocent/auth/repository/UserRepository.java
@@ -2,11 +2,21 @@ package dev.Innocent.auth.repository;
 
 import dev.Innocent.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String username);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.password = ?2 where u.email = ?1")
+    void updatePassword(String email, String password);
 }
