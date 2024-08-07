7adc46ce8650bb4886224dd9242a7de5bcf6ffd5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-07 13:30:26 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/controller/OrderController.java
@@ -0,0 +1,11 @@
+package dev.Innocent.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+}
