09a12e80a6b3c43853d8d87054d6b1d0badb90b6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-10 14:26:44 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/controller/OrderController.java
+++ Viking-Treading/src/main/java/dev/Innocent/controller/OrderController.java
@@ -1,11 +1,15 @@
 package dev.Innocent.controller;
 
+import dev.Innocent.DTO.request.CreateOrderRequest;
+import dev.Innocent.model.Coin;
+import dev.Innocent.model.Order;
+import dev.Innocent.model.User;
 import dev.Innocent.service.CoinService;
 import dev.Innocent.service.OrderService;
 import dev.Innocent.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,4 +20,12 @@ public class OrderController {
     private final CoinService coinService;
 
 
+    @PostMapping("/pay")
+    public ResponseEntity<Order> payOrderPayment(@RequestHeader("Authorization") String jwt,
+                                                 @RequestBody CreateOrderRequest createOrderRequest) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Coin coin = coinService.findById(createOrderRequest.getCoinId());
+        Order order = orderService.processOrder(coin, createOrderRequest.getQuantity(), createOrderRequest.getOrderType(), user);
+        return ResponseEntity.ok(order);
+    }
 }
