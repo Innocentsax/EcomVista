8e8660cbd52ecae0e326c54ba337830be3da616c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-11 12:19:27 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/service/Impl/AssetServiceImpl.java
@@ -0,0 +1,59 @@
+package dev.Innocent.service.Impl;
+
+import dev.Innocent.model.Asset;
+import dev.Innocent.model.Coin;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.AssetRepository;
+import dev.Innocent.service.AssetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AssetServiceImpl implements AssetService {
+    private final AssetRepository assetRepository;
+
+    @Override
+    public Asset createAsset(User user, Coin coin, double quantity) {
+        Asset asset = new Asset();
+        asset.setUser(user);
+        asset.setCoin(coin);
+        asset.setQuantity(quantity);
+        asset.setBuyPrice(coin.getCurrentPrice());
+        return assetRepository.save(asset);
+    }
+
+    @Override
+    public Asset getAssetById(Long assetId) {
+        return assetRepository.findById(assetId).orElseThrow(()-> new RuntimeException("Asset not found"));
+    }
+
+    @Override
+    public Asset getAssetByUserIdAndId(Long userId, Long assetId) {
+        return null;
+    }
+
+    @Override
+    public List<Asset> getUsersAssets(Long userId) {
+        return assetRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Asset updateAsset(Long assetId, double quantity) {
+        Asset oldAsset = getAssetById(assetId);
+        oldAsset.setQuantity(quantity + oldAsset.getQuantity());
+        return assetRepository.save(oldAsset);
+    }
+
+    @Override
+    public Asset findAssetByUserIdAndCoinId(Long userId, String coinId) {
+        return assetRepository.findByUserIdAndCoinId(userId, coinId);
+    }
+
+    @Override
+    public void deleteAsset(Long assetId) {
+        assetRepository.deleteById(assetId);
+    }
+}
