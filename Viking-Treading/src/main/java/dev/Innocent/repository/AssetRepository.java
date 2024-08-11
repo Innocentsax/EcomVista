c1791cfa2f531cc93cf41f13e5b65c271b959c24
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-11 12:19:18 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/AssetRepository.java
@@ -0,0 +1,11 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssetRepository extends JpaRepository<Asset, Long> {
+    List<Asset> findByUserId(Long userId);
+    Asset findByUserIdAndCoinId(Long userId, String coinId);
+}
