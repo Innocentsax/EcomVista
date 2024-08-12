66e8cfd436d75725da67ee180cce7e060459daed
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-12 19:02:23 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/PaymentDetailsRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.PaymentDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
+    PaymentDetails findByUserId(Long userId);
+}
