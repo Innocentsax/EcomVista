dd56f69b9eb1807d18e14c25a3eeb9965f41b09f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-18 14:37:00 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/model/Order.java
+++ Viking-Treading/src/main/java/dev/Innocent/model/Order.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
