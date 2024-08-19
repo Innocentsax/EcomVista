3f38b3a16089f9736236c71d24085aee8bd3a872
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-19 14:08:01 +0100

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/PaymentOrderRepository.java
@@ -0,0 +1,9 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+}
