2af863d848ff81a2e1ce1d19905d4689c75ca861
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-20 08:48:58 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/model/User.java
+++ Viking-Treading/src/main/java/dev/Innocent/model/User.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
