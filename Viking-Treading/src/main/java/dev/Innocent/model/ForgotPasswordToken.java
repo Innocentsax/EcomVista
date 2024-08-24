92dd79b1406274790dde0c858a85962417857126
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-24 14:47:16 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/model/ForgotPasswordToken.java
+++ Viking-Treading/src/main/java/dev/Innocent/model/ForgotPasswordToken.java
@@ -3,17 +3,19 @@ package dev.Innocent.model;
 import dev.Innocent.enums.VerificationType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "forgot_password_token")
 public class ForgotPasswordToken {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String otp;
     private VerificationType verificationType;
