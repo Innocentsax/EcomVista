ec59f1259519b43c988fd86e68190927ef593087
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-08-25 14:58:52 +0100

    Viking Treading SetUp

--- Viking-Treading/src/main/java/dev/Innocent/controller/ChatController.java
+++ Viking-Treading/src/main/java/dev/Innocent/controller/ChatController.java
@@ -1,6 +1,13 @@
 package dev.Innocent.controller;
 
+import dev.Innocent.DTO.request.PromptRequest;
+import dev.Innocent.DTO.response.AIChatResponse;
+import dev.Innocent.service.ChatbotService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ai/chat")
 @RequiredArgsConstructor
 public class ChatController {
+    private final ChatbotService chatbotService;
+
+    @PostMapping
+    public ResponseEntity<AIChatResponse> getCoinDetails(@RequestBody PromptRequest promptRequest) throws Exception {
+        AIChatResponse aiChatResponse = chatbotService.getCoinDetails(promptRequest.getPrompt());
+        return new ResponseEntity<>(aiChatResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/simple")
+    public ResponseEntity<String> getSimpleResponse(@RequestBody PromptRequest promptRequest) {
+        String response = chatbotService.simpleChat(promptRequest.getPrompt());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
