1cda8e2e88b0756a93b750945bbb7243581c0b7d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-01 20:11:15 +0800

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/DTO/response/FunctionResponse.java
@@ -0,0 +1,10 @@
+package dev.Innocent.DTO.response;
+
+import lombok.Data;
+
+@Data
+public class FunctionResponse {
+    private String currencyName;
+    private String functionName;
+    private String currencyData;
+}
