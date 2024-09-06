eb346cc6d71877bb32d562c1487ce7d32843d97a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-09-06 21:10:48 +0800

    Viking Treading SetUp

---
+++ Viking-Treading/src/main/java/dev/Innocent/repository/WithdrawalRepository.java
@@ -0,0 +1,10 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.Withdrawal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WithdrawalRepository extends JpaRepository<Withdrawal, Long> {
+    List<Withdrawal> findByUserId(Long userId);
+}
