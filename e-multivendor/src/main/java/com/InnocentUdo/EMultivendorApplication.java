06cafbb200e3c028fbea4eef68f3142d2518b6d1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-01-29 13:17:38 +0800

    E-commerce Multiple Vendors

---
+++ e-multivendor/src/main/java/com/InnocentUdo/EMultivendorApplication.java
@@ -0,0 +1,13 @@
+package com.InnocentUdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMultivendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMultivendorApplication.class, args);
+	}
+
+}
---
+++ e-multivendor/src/test/java/com/InnocentUdo/EMultivendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InnocentUdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMultivendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
