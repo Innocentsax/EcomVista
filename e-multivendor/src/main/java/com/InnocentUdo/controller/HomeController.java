624af093b9b3f356856e2541592253e9192f37eb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-22 23:38:36 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/controller/HomeController.java
+++ e-multivendor/src/main/java/com/InnocentUdo/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.InnocentUdo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "Welcome to ecommerce multi vendor system";
+    }
 }
