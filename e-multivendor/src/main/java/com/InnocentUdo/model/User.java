a880a5ddb7ea0181c1cc378e5b7e34e528f1a790
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-25 21:34:14 +0800

    E-commerce Multiple Vendors

---
+++ e-multivendor/src/main/java/com/InnocentUdo/model/User.java
@@ -0,0 +1,18 @@
+package com.InnocentUdo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+}
