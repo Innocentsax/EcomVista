ab68e59aef1d3feab1363c05d4544bcd47271f4a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-27 15:55:16 +0800

    Entity mapping

---
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Coupon.java
@@ -0,0 +1,14 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
