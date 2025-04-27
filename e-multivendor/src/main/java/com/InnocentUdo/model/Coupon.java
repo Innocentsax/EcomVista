54813c6d63c640d131580ad4d585cb68b3cd36a2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-27 17:32:44 +0800

    Entity mapping

--- e-multivendor/src/main/java/com/InnocentUdo/model/Coupon.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Coupon.java
@@ -4,11 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Coupon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String code;
+    private double discountPercentage;
+    private LocalDate validityStartDate;
+    private LocalDate validityEndDate;
 }
