30fce8b551b6710f77ab81945cd73a7e1c4c9db6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-27 17:32:40 +0800

    Entity mapping

--- e-multivendor/src/main/java/com/InnocentUdo/model/Address.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Address.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Address {
 
     @Id
