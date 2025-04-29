c66c11bb8aea3a7114efaa27e2dd1bc7fa167a53
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-04-29 23:07:07 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/DTO/response/ApiResponse.java
+++ e-multivendor/src/main/java/com/InnocentUdo/DTO/response/ApiResponse.java
@@ -1,8 +1,12 @@
 package com.InnocentUdo.DTO.response;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse {
     private String message;
 
