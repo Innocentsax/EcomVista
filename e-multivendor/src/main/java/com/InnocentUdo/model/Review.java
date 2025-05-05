12510a3ac67302fdc5d5050d260b00fcb4e185bb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-05 22:44:21 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/Review.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Review.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -29,6 +30,13 @@ public class Review {
 
     @JsonIgnore
     @ManyToOne
-    @JoinColumn(name = "product_id", nullable = false)
+    @Column(nullable = false)
     private Product product;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
