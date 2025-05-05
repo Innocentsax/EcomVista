5360eec090f880151e3d296009db7c81c7173ff0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-05 22:44:16 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/Seller.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Seller.java
@@ -1,9 +1,8 @@
 package com.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.InnocentUdo.domain.AccountStatus;
+import com.InnocentUdo.domain.USER_ROLE;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -17,4 +16,27 @@ public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String sellerName;
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupAddress = new Address();
+
+    private String GSTIN;
+
+    private USER_ROLE role = USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
 }
