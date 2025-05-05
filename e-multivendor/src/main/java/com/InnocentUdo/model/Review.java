e421cf4fea7d03474ecc05ae6fa67500f7837b1f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-05 21:23:45 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/Review.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Review.java
@@ -1,7 +1,10 @@
 package com.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -11,4 +14,21 @@ import lombok.*;
 @EqualsAndHashCode
 public class Review {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewText;
+
+    @Column(nullable = false)
+    private double rating;
+
+    @ElementCollection
+    private List<String> productImages;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 }
