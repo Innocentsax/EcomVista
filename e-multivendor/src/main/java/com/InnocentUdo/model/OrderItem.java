54caff4bbcd28e74c4ca62d20180b59e6a944e45
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-10 13:48:36 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/OrderItem.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/OrderItem.java
@@ -1,9 +1,6 @@
 package com.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -17,4 +14,12 @@ public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private Product product;
+    private String size;
+    private int quantity;
+    private Integer mrpPrice;
+    private Integer sellingPrice;
+    private Long userId;
 }
