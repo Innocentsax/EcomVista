0affded6033d3dd524f42c1511888399c0a8ec7b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-11 10:02:41 +0800

    E-commerce Multiple Vendors

---
+++ e-multivendor/src/main/java/com/InnocentUdo/model/Transaction.java
@@ -0,0 +1,30 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @OneToOne
+    private Order order;
+
+    @ManyToOne
+    private Seller seller;
+
+    private LocalDateTime date = LocalDateTime.now();
+}
