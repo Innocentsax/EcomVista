4c5e70bced6e52dfa0a1bc934a4ae7977cf6004f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-12 18:21:31 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/VerificationCode.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/VerificationCode.java
@@ -1,9 +1,6 @@
 package com.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -17,4 +14,13 @@ public class VerificationCode {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String otp;
+    private String email;
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Seller seller;
 }
