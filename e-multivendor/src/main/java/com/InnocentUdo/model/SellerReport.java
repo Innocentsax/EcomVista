3f4caf5b9147994fe438ca149cc6d0138af68b25
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-13 21:48:42 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/SellerReport.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/SellerReport.java
@@ -1,9 +1,6 @@
 package com.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -11,12 +8,20 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
 public class SellerReport {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @OneToOne
+    private Seller seller;
+
+    private Long totalEarnings = 0L;
+    private Long totalSales = 0L;
+    private Long totalRefunds = 0L;
+    private Long totalTax = 0L;
+    private Long netEarnings = 0L;
+    private Integer totalOrders = 0;
 
 }
