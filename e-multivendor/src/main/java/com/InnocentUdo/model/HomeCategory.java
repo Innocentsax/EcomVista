63e7439fc8fa9a3271ba01854bdfeb94e4a57db8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-15 00:09:19 +0800

    E-commerce Multiple Vendors

---
+++ e-multivendor/src/main/java/com/InnocentUdo/model/HomeCategory.java
@@ -0,0 +1,24 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class HomeCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String image;
+    private String categoryId;
+    private HomeCategorySection section;
+}
