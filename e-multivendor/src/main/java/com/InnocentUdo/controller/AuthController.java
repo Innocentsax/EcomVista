153fbd2a724c3d6f04a6c5826f80a4e6efc04c57
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-17 21:50:10 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/controller/AuthController.java
+++ e-multivendor/src/main/java/com/InnocentUdo/controller/AuthController.java
@@ -1,4 +1,13 @@
 package com.InnocentUdo.controller;
 
+import com.InnocentUdo.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    public ResponseEntity<User> createUserHandler(){
+        return null;
+    }
 }
