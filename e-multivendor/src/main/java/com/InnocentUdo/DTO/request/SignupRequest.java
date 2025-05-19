de1ee360dc8f7733d7b746b6931b0c0121b83c42
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-19 21:05:56 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/DTO/request/SignupRequest.java
+++ e-multivendor/src/main/java/com/InnocentUdo/DTO/request/SignupRequest.java
@@ -1,5 +1,11 @@
 package com.InnocentUdo.DTO.request;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupRequest {
     private String email;
     private String fullName;
