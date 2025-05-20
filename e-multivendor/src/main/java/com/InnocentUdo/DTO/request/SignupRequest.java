3ff8f19615e54ea93fa5d38cb6a8b670a6ce24cb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-20 22:14:20 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/DTO/request/SignupRequest.java
+++ e-multivendor/src/main/java/com/InnocentUdo/DTO/request/SignupRequest.java
@@ -10,4 +10,28 @@ public class SignupRequest {
     private String email;
     private String fullName;
     private String otp;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
 }
