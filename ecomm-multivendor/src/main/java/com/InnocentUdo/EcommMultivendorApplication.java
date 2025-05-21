454b330ac1ccad43f9926b843e2b29c50f9a2329
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-21 22:06:45 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/EcommMultivendorApplication.java
@@ -0,0 +1,13 @@
+package com.InnocentUdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommMultivendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommMultivendorApplication.class, args);
+	}
+
+}
