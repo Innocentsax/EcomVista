420fa84663c8d190a9bdfdd7b2c24d3eeab8614b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-21 19:37:18 +0800

    E-commerce Multiple Vendors

--- e-multivendor/src/main/java/com/InnocentUdo/model/User.java
+++ e-multivendor/src/main/java/com/InnocentUdo/model/User.java
@@ -35,4 +35,69 @@ public class User {
     @ManyToMany
     @JsonIgnore
     private Set<Coupon> usedCoupons = new HashSet<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public USER_ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Set<Coupon> getUsedCoupons() {
+        return usedCoupons;
+    }
+
+    public void setUsedCoupons(Set<Coupon> usedCoupons) {
+        this.usedCoupons = usedCoupons;
+    }
 }
