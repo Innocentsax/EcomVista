bbc53b63527da59aa581f0006743bb04e3e310b6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-22 22:03:33 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Cart.java
@@ -0,0 +1,31 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> cartItem = new HashSet<>();
+    private double totalSellingPrice;
+    private int totalItem;
+    private int totalMrpPrice;
+    private int discount;
+    private String couponCode;
+}
