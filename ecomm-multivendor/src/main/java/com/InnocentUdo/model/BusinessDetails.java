5471f1fb71c591d5fcd33cc4a41b0061eef57a9c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-22 22:03:29 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/BusinessDetails.java
@@ -0,0 +1,15 @@
+package com.InnocentUdo.model;
+
+import lombok.Data;
+
+
+@Data
+public class BusinessDetails {
+
+    private String businessName;
+    private String businessEmail;
+    private String businessMobile;
+    private String businessAddress;
+    private String logo;
+    private String banner;
+}
