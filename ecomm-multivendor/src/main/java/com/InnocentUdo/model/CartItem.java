2678759e918ede188a77effab3c2d9ef3dce92ab
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-23 08:51:53 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/CartItem.java
@@ -0,0 +1,30 @@
+package com.InnocentUdo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+    private int quantity = 1;
+    private String size;
+    private Integer mrpPrice;
+    private Integer sellingPrice;
+    private Long userId;
+}
