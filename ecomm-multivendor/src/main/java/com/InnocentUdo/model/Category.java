63c6a4e3d1ca5e0dc4607363b945eba40ea442bb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-23 08:51:57 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Category.java
@@ -0,0 +1,30 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String categoryId;
+
+    @ManyToOne
+    private Category parentCategory;
+
+    @NotNull
+    private Integer level;
+}
