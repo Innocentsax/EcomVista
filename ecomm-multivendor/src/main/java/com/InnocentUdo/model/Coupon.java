76782d52ad70ffca0ba300082bccdefc8a0ed597
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-23 08:52:01 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Coupon.java
@@ -0,0 +1,31 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String code;
+    private double discountPercentage;
+    private LocalDate validityStartDate;
+    private LocalDate validityEndDate;
+    private double minimumOrderValue;
+    private boolean isActive = true;
+
+    @ManyToMany(mappedBy = "usedCoupons")
+    private Set<User> userByUsers = new HashSet<>();
+}
