309c5b69fe9ca2d92d9471055bdb9175380c99f2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-24 14:06:12 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Order.java
@@ -0,0 +1,48 @@
+package com.InnocentUdo.model;
+
+import com.InnocentUdo.domain.OrderStatus;
+import com.InnocentUdo.domain.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String orderId;
+
+    @ManyToOne
+    private User user;
+    private Long sellerId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @ManyToOne
+    private Address shippingAddress;
+
+    @Embedded
+    private PaymentDetails paymentDetails = new PaymentDetails();
+
+    private double totalMrpPrice;
+    private Integer totalSellingPrice;
+    private Integer discount;
+    private OrderStatus orderStatus;
+    private int totalItem;
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
+    private LocalDateTime orderDate = LocalDateTime.now();
+    private LocalDateTime deliverDate = orderDate.plusDays(7);
+}
