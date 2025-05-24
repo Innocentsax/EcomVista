d7e49211fafac99a08acef0b54aecbd75b2ec876
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-24 14:06:17 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/OrderItem.java
@@ -0,0 +1,30 @@
+package com.InnocentUdo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+    private String size;
+    private int quantity;
+    private Integer mrpPrice;
+    private Integer sellingPrice;
+    private Long userId;
+}
