93230a54f6ac1c3be45abddfc4f591c73f0971ac
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-24 14:06:24 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/PaymentDetails.java
@@ -0,0 +1,12 @@
+package com.InnocentUdo.model;
+
+import com.InnocentUdo.domain.PaymentStatus;
+
+public class PaymentDetails {
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayPaymentLinkReferenceId;
+    private String razorpayPaymentLinkStatus;
+    private String razorpayPayment;
+    private PaymentStatus status;
+}
