730b527975bd0729b377bb9175361da13258dbf1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-25 16:58:27 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Product.java
@@ -0,0 +1,44 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+    private int mrpPrice;
+    private int sellingPrice;
+    private int discountPercent;
+    private int quantity;
+    private String color;
+
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+
+    private int numRatings;
+    @ManyToOne
+    private Category category;
+    @ManyToOne
+    private Seller seller;
+    private LocalDateTime createdAt;
+
+//    @ElementCollection
+    private String sizes;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+}
