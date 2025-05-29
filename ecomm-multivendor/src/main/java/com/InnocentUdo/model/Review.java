158322dd08e4ee92206015c9660f100547fcfa9f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-29 21:31:52 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Review.java
@@ -0,0 +1,40 @@
+package com.InnocentUdo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewText;
+
+    @Column(nullable = false)
+    private double rating;
+
+    @ElementCollection
+    private List<String> productImages;
+
+    @JsonIgnore
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
