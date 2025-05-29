c844f8f03105506f826f927b9d1c5290e9573be2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-29 21:32:21 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Wishlist.java
@@ -0,0 +1,26 @@
+package com.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @ManyToMany
+    private Set<Product> products = new HashSet<>();
+}
