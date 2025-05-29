333550a9c2e43405db7a446cd210e0f8d332da24
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-29 21:32:07 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/User.java
@@ -0,0 +1,38 @@
+package com.InnocentUdo.model;
+
+import com.InnocentUdo.domain.USER_ROLE;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private String email;
+    private String fullName;
+    private String mobile;
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+
+    @OneToMany
+    private Set<Address> addresses = new HashSet<>();
+
+    @ManyToMany
+    @JsonIgnore
+    private Set<Coupon> usedCoupons = new HashSet<>();
+}
