d78b5afcddfb00c0d6a51c9c584315c59f0e04a2
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-29 21:31:48 +0800

    Ecomm Project

---
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/model/Seller.java
@@ -0,0 +1,42 @@
+package com.InnocentUdo.model;
+
+import com.InnocentUdo.domain.AccountStatus;
+import com.InnocentUdo.domain.USER_ROLE;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String sellerName;
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupAddress = new Address();
+
+    private String GSTIN;
+
+    private USER_ROLE role = USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
+}
