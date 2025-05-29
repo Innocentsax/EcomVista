14e06439af05865c83677839cd7a1b9705fe9962
INNOCENT UDO <innocentcharlesudo@gmail.com>
2025-05-29 21:31:41 +0800

    Ecomm Project

--- ecomm-multivendor/src/main/java/com/InnocentUdo/ecommMultivendorApplication.java
+++ ecomm-multivendor/src/main/java/com/InnocentUdo/ecommMultivendorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommMultivendorApplication {
+public class ecommMultivendorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommMultivendorApplication.class, args);
+		SpringApplication.run(ecommMultivendorApplication.class, args);
 	}
 
 }
